2ac0c26a73b6059bfcbf77a383a4ee8d2ed72d14
minsu <a2238737@gmail.com>
2024-08-20 18:00:51 +0900

    Feat: startup API 정렬 기준 추가

--- API/combined.js
+++ API/combined.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import startupApp from "./startup.js";
-import userApp from "./User.js";
+import userApp from "./user.js";
 import cors from "cors";
 
 dotenv.config();
@@ -10,10 +10,8 @@ const mainApp = express();
 mainApp.use(cors());
 mainApp.use(express.json());
 
-// `startup.js`에서 정의된 라우트들을 `/` 경로에 연결
 mainApp.use("/", startupApp);
 
-// `user.js`에서 정의된 라우트들을 `/` 경로에 연결
 mainApp.use("/", userApp);
 
 mainApp.listen(3000, () => {
